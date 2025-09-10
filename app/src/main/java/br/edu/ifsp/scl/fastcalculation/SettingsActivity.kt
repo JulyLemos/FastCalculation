0b3cf2fdb5f62d732742308a32efbac6535d9fc6
July <julylemos1@hotmail.com>
2025-09-10 14:49:37 -0300

    Criação do trabalho

---
+++ app/src/main/java/br/edu/ifsp/scl/fastcalculation/SettingsActivity.kt
@@ -0,0 +1,49 @@
+package br.edu.ifsp.scl.fastcalculation
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.fastcalculation.Extras.EXTRA_SETTINGS
+import br.edu.ifsp.scl.fastcalculation.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+    private val activitySettingsBinding: ActivitySettingsBinding by lazy {
+        ActivitySettingsBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(activitySettingsBinding.root)
+
+        setSupportActionBar(activitySettingsBinding.gameTbIn.gameTb)
+        supportActionBar?.title = getString(R.string.app_name)
+        supportActionBar?.subtitle = getString(R.string.settings)
+
+        activitySettingsBinding.apply {
+            letsGoBt.setOnClickListener {
+                val settings = Settings(
+                    playerNameEt.text.toString(),
+                    (roundsSp.selectedView as TextView).text.toString().toInt(),
+                    roundsIntervalRg.run {
+                        findViewById<RadioButton>(checkedRadioButtonId).text.toString().toLong() * 1000L
+                    }
+                )
+                val gameActivityIntent = Intent(this@SettingsActivity, GameActivity::class.java)
+                gameActivityIntent.putExtra(EXTRA_SETTINGS, settings)
+                startActivity(gameActivityIntent)
+                finish()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
